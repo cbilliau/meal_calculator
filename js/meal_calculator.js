1c4fd600435a67c8976f7b7e81e170c87d550fd3
cbilliau <christopher@billiau.com>
2016-06-28 22:55:24 -0400

    Finished controller functions for generating bill sof rindividual diners. Next - controllers for calc total table bill then.

--- js/meal_calculator.js
+++ js/meal_calculator.js
@@ -2,7 +2,7 @@
 
 // Menu object
 const MENU = {
-    Fish: 13.99,
+    Fish: 9.99,
     Shrimp: 13.99,
     Clams: 14.99,
     Oysters: 17.99
@@ -19,16 +19,16 @@ class Diner {
         this.billDiner = 0;
         this.billTipDiner = 0;
         this.taxDue = 0;
-				this.billTotalDiner = 0;
+        this.billTotalDiner = 0;
     }
 
     // Randomly choose one dish from menu
     chooseRandomDish(num, menu) {
         let dish = [];
-        let dishes = Object.keys(menu);
-        for (var i = 0; i <= num; i++) {
-          let choice = dishes[Math.floor(Math.random() * dishes.length)];
-          dish.push(menu[choice]);
+        let plates = Object.keys(menu);
+        for (var i = 1; i <= num; i++) {
+            let choice = plates[Math.floor(Math.random() * plates.length)];
+            dish.push(menu[choice]);
         }
         return dish;
     }
@@ -41,27 +41,32 @@ class Diner {
     // Calc total cost of dishes chosen (subTotal)
     calculateSubTotalDiner(dishes) {
         let subTotal = 0;
-        for (var el in dishes) {
-            if (dishes.hasOwnProperty(el)) {
-                subTotal += parseFloat(dishes[el]);
-            }
+        for( var el in dishes) {
+          if (dishes.hasOwnProperty(el)) {
+            subTotal += dishes[el];
+          }
         }
-        return subTotal;
+        return subTotal.toFixed(2);
     }
 
     // Calc tip based on subTotal
     calculateTipDiner(subTotal, tipPerc) {
-        return (subTotal * tipPerc);
+        return (subTotal * tipPerc).toFixed(2);
     }
 
     // Calc tax bill for diner
     calculateTaxDiner(subTotal, salesTax) {
-        return (subTotal * salesTax);
+        return (subTotal * salesTax).toFixed(2);
     }
 
     // Calc total bill due for diner
     calculateBillTotalDiner(subTotal, tip, tax) {
-        return (subTotal + tip + tax);
+        let bill = Number(subTotal);
+        let taxes = Number(tax);
+        let tips = Number(tip);
+        let total = (bill + taxes + tips);
+        console.log(total);
+        return total.toFixed(2);
     }
 
 }
@@ -87,52 +92,78 @@ class Bill {
         this.billTipTable = 0;
     }
 
-		// Routine to calc diner totals
-		// Use for subTotal, tip, and tax
-		calculateTotalsTable() {
-				let sum = 0;
-				for (var i=0; i < arguments.length; i++) {
-					sum += parseInt(arguments[i]);
-				}
-				return sum;
-		}
+    // Routine to calc diner totals
+    // Use for subTotal, tip, and tax
+    calculateTotalsTable() {
+        let sum = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            sum += parseInt(arguments[i]);
+        }
+        return sum;
+    }
 }
 
 class View {
-		constructor() {}
+    constructor() {}
 }
 
 class Controller {
 
-		constructor(bill, view) {
-			this.bill = bill;
-			this.view = view;
-		}
-
-		chooseDinerMeal(diner, num, menu) {
-      return diner.chooseRandomDish(num, menu);
+    constructor(bill, view) {
+            this.bill = bill;
+            this.view = view;
+        }
+        // Get rndm dishes for
+    chooseMeals(diner, num, menu) {
+        return diner.chooseRandomDish(num, menu);
     }
 
-    recordMealChoices(diner, dishes) {
-      diner.dishes.push(dishes);
+    recordMeal(diner, dishes) {
+        diner.storeDishDecision(dishes);
     }
 
+    getsubTotal(diner, dishes) {
+      return diner.calculateSubTotalDiner(dishes);
+    }
 
+    getTipDue(diner, subTotal) {
+      return diner.calculateTipDiner(subTotal, diner.tipPerc);
+    }
+
+    getTaxDue(diner, subTotal) {
+      return diner.calculateTaxDiner(subTotal, diner.salesTax);
+    }
+
+    getBillTotal(diner, subTotal, tip, tax) {
+      return diner.calculateBillTotalDiner(subTotal, tip, tax);
+    }
 }
 
 // Instances
+// Create diners
 const diner1 = new Diner(MENU);
 const diner2 = new Diner(MENU);
-const table = new Table (diner1, diner2); // Holds diners
+
+//Create table, bill, andcontroller
+const table = new Table(diner1, diner2); // Holds diners
 const bill = new Bill(table); // Holds table (diner 1 & diner 2)
 const view = new View(); // To be entered into controller
 const controller = new Controller(bill, view); // Holds Bill (table)
 
 
-let dish = controller.chooseDinerMeal(diner1, 2, MENU);
-controller.recordMealChoices(diner1,dish);
-console.log(dish);
-console.log(diner1.dishes);
-console.log(table);
-console.log(bill);
-console.log(controller);
+let dishes = controller.chooseMeals(diner1, 2, MENU);
+controller.recordMeal(diner1, dishes);
+let subTotal = controller.getsubTotal(diner1, dishes);
+let tip = controller.getTipDue(diner1, subTotal);
+let tax = controller.getTaxDue(diner1, subTotal);
+let billTotal = controller.getBillTotal(diner1, subTotal, tip, tax);
+
+
+console.log('subTotal $' + subTotal);
+console.log('tax $' + tax);
+console.log('tip $' + tip);
+console.log('bill total $' + billTotal);
+//
+// console.log(table);
+// console.log(bill);
+// console.log(controller);
